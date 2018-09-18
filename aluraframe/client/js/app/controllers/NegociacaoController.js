2484cca139de42c624c843033bb4ba76f832e4d2
Renan <developer.renansoares@gmail.com>
2018-09-18 09:31:45 -0300

    template strings

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,20 +7,36 @@ class NegociacaoController{
 		this._inputData = $("#data");
 		this._inputQuantidade = $("#quantidade");
 		this._inputValor = $("#valor");
+		this._listaNegociacoes = new ListaNegociacoes();
 		Object.freeze(this);
 	}
 
 	adiciona(event){
 		event.preventDefault(); //cancela a submissao dos dados do formulario para capturar esses dados
 
-		let helper = new DateHelper();
+		//let helper = new DateHelper();
 		
-		//cria uma negociacao com os seus parametros
-		let negociacao = new Negociacao (helper.textoParaData(this._inputData.value), this._inputQuantidade.value,this._inputValor.value);
 
-		console.log(helper.dataParaTexto(negociacao.data));
+		this._listaNegociacoes.adiciona(this._criaNegociacao());
+		this._limpaFormulario();
+
+		console.log(this._listaNegociacoes.negociacoes);
 		
 		
+	}
+
+	_criaNegociacao(){
+		//cria uma negociacao com os seus parametros
+		return new Negociacao (DateHelper.textoParaData(this._inputData.value), this._inputQuantidade.value,this._inputValor.value);
+	}
+
+	_limpaFormulario(){
+		this._inputData.value ='';
+		this._inputQuantidade.value =1;
+		this._inputValor.value = 0.0;
+
+		this._inputData.focus();
+
 	}
 
 }
--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,12 +1,26 @@
 class DateHelper{
 
-
-	textoParaData(texto){
-		return new Date(...texto.split('-').map((item, indice) =>  item - indice % 2));
+	constructor(){
+		throw new Error ('DateHelper nao pode ser instanciada!');
 	}
 
-	dataParaTexto(data){
-		return data.getDate() + "/" + (data.getMonth()+1) + "/" + data.getFullYear();
+	static textoParaData(texto){
+		//arrow function: a flecha pode retirar a palavra reservada function. Pode emitir o bloco {} se tiver uma unica instrução. Pode tirar o ; tbm
+		return new Date(...texto.split('-').map((item, indice) =>  item - indice % 2));
+
+		/*código equivalente*/
+		// let data = new Date(...this._inputData.value.split('-').map(function(item, indice)  { 
+			
+		// 	return item - indice % 2;
+		// }));
+		///console.log(data);
+	}
+
+	static dataParaTexto(data){
+	//	if(!/\d{4}-\d{2}-\d{2}/.test(data)) throw new Error ("Deve estar no formato aaaa-mm-dd");
+
+		return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+		
 	}
 
 
