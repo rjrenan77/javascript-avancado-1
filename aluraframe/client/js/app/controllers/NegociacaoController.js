f7e72288b50337d01d2a21e86e6b08ba7e027b5a
Renan <developer.renansoares@gmail.com>
2018-10-01 16:50:39 -0300

    introducao a requisica no servidor . ver MEAN

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -10,8 +10,7 @@ class NegociacaoController{
 
 		
 		this._listaNegociacoes = new Bind(new ListaNegociacoes(), new NegociacoesView($("#negociacoesView")),'adiciona','esvazia');
-
-		this._mensagem = new Bind(new Mensagem(), new MensagemView($("#mensagemView")),'texto');
+		this._mensagem = new Bind(new Mensagem(), new MensagemView($("#mensagemView"))  ,'texto');
 		
 		Object.freeze(this);
 	}
@@ -25,21 +24,11 @@ class NegociacaoController{
 	}
 
 	adiciona(event){
+	
 		event.preventDefault(); //cancela a submissao dos dados do formulario para capturar esses dados
-
-		//let helper = new DateHelper();
-		
-
 		this._listaNegociacoes.adiciona(this._criaNegociacao());
-				
 		this._mensagem.texto = 'Negociação adicionada com sucesso!';
 		this._limpaFormulario();
-
-
-
-		//console.log(this._listaNegociacoes.negociacoes);
-		
-		
 	}
 
 	_criaNegociacao(){
@@ -54,6 +43,53 @@ class NegociacaoController{
 
 		this._inputData.focus();
 
+	}
+
+	importaNegociacoes(){
+
+		
+		let xhr =  new XMLHttpRequest();
+
+		xhr.open('GET', 'negociacoes/semana');
+
+		/*configurações*/
+		xhr.onreadystatechange = () => {
+			
+			/*
+			0: requisição ainda não iniciada
+
+			1: conexão com o servidor estabelecida
+
+			2: requisição recebida
+
+			3: processando requisição
+
+			4: requisição está concluída e a resposta está pronta
+			*/
+
+			if(xhr.readyState == 4){
+				if(xhr.status == 200){
+					console.log('obtendo as negociações do servidor...');
+					console.log(xhr.responseText);
+
+					JSON.parse(xhr.responseText) //transformando JSON em array
+					.map((objeto)=>new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)) // cada objeto retornado do json eu converto em um array de negociacao
+					.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao)); //percorro cada item e adiciono na lista de Negociacoes
+
+					this._mensagem.texto = 'Negociações importadas com sucesso!';
+
+					console.log('feito!');
+				}else{
+					console.log('Não foi possível obter as negociações do servidor!');
+					console.log(xhr.responseText);
+					this._mensagem.texto = "Não foi possível obter as negociações.";
+				}
+			}
+
+		};
+
+		xhr.send();
+	
 	}
 
 }
